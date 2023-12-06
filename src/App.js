3755209916ef58b27e2848081eb5e6c450a76097
molano5858 <molano5858@gmail.com>
2023-12-06 17:54:45 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const App = () => {
       <div className='category-container'>
         {/* <img /> */}
         <div className='category-body-container'>
+          <h2>Felipe molano</h2>
           <h2>Hats</h2>
           <p>Shop Now</p>
         </div>
