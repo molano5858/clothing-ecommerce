e203bd5559200f562e14a8173521c94386092eb1
molano5858 <molano5858@gmail.com>
2023-12-12 17:41:27 -0500

    Creating Routes and navigation bar

---
+++ src/components/routes/home/prueba.jsx
@@ -0,0 +1,12 @@
+
+
+const Prueba = () => {
+  
+  
+
+  return (
+    <h1>Pagina de prueba</h1>
+  );
+};
+
+export default Prueba
---
+++ src/components/routes/navigation/navigation.jsx
@@ -0,0 +1,21 @@
+import {Outlet,Link } from 'react-router-dom' 
+import React from 'react'
+import './navigation.styles.scss'
+import {ReactComponent as CrwnLogo} from '../../../assets/crown.svg'
+const Navigation = ()=>{
+    return (
+      <React.Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to={'/'}>
+                <CrwnLogo />
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to={'/prueba'}>Shop</Link>
+            </div>
+        </div>
+        <Outlet />
+      </React.Fragment>
+    )
+  }
+
+export default Navigation
